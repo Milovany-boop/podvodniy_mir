5d1deee0f2331d5f88aff57c3f7b09c1b80b9fb1
milovany-boop <moldovanov.matveyka@gmail.com>
2025-10-23 21:58:06 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// firebase-config.js
+const firebaseConfig = {
+    apiKey: "AIzaSyCp03ZLPujUBnXlDqvXwDj6MknBCURrFok",
+    authDomain: "oscar-voting-2025.firebaseapp.com",
+    projectId: "oscar-voting-2025",
+    storageBucket: "oscar-voting-2025.firebasestorage.app",
+    messagingSenderId: "651906170001",
+    appId: "1:651906170001:web:85580893526614d16bd330"
+};
